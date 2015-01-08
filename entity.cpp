a7cc4c924e34cb92b52fc56eb77f535d5c9e01bc
Schmitt <clement.flance@gmail.com>
2015-01-08 02:35:20 +0100

    Ajout d'élément de la classe Entité

--- entity.cpp
+++ entity.cpp
@@ -1,5 +1,26 @@
 #include "entity.h"
 
+using namespace std;
+
 Entity::Entity()
 {
+}
+
+Entity::Entity(QString s)
+{
+}
+
+Entity::getPoint(int i){return point[i];}
+
+Entity::getFaces(int i){return faces[i];}
+
+Entity::getPpf(int i){return ppf[i];}
+
+Entity::getNbFaces(){return nbFaces;}
+
+Entity::getNbPoints(){return nbPoints;}
+
+Entity::LoadModel(Qstring s)
+{
+
 }
--- entity.h
+++ entity.h
@@ -1,10 +1,38 @@
 #ifndef ENTITY_H
 #define ENTITY_H
 
+#include <QVector3D>
+
 class Entity
 {
 public:
     Entity();
+    Entity(QString); //Récupère les données d'un modèle 3D avec son localPath
+
+    ~Entity();
+
+    QVector3D getPoint(int);
+    int* getFaces(int);
+    int getPpf(int);
+    int getNbFaces();
+    int getNbPoints();
+
+    void LoadModel(QString);
+
+private:
+    QVector3D *point; // ensemble des points de notre objet
+    int** faces; //ensemble des faces de notre objet
+    int* ppf; //nombre points par face (ppf)
+    int nbFaces; //nombre de face dans notre objet
+    int nbPoints; //nombre de points dans notre objet
+
+    QVector3D position; // position du modèle 3D dans la scène
+    QVector3D force; // total des forces appliqués à notre entité
+
+    float velocity;
+    float massValue;
+
+    bool useGravity;
 };
 
 #endif // ENTITY_H
