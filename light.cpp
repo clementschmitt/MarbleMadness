530320a739ebb9a1db33cc2bef03815f60f2d4bd
Schmitt <clement.flance@gmail.com>
2015-01-11 20:46:31 +0100

    Gestion de l'atténuation de la lumière
    Cette gestion est gérée linéairement. C'est à dire qu'un vertex situé à
    mi-distance du rayon de la lumière recevra la moitié de sa luminosité.

--- light.cpp
+++ light.cpp
@@ -12,7 +12,18 @@ void Light::setRadius(float r){radius = r;}
 void Light::setColor(int r, int g, int b){color = QColor(r, g, b);}
 void Light::setPosition(QVector3D p){position = p;}
 
+/**
+ * @brief Light::computeLighting
+ * @param p du vecteur à illuminer
+ * @param normal du vecteur à illuminer
+ * @return
+ */
 QColor Light::computeLighting(QVector3D p, QVector3D normal)
 {
+    //On calcul le vecteur entre le point et la lumière
+    QVector3D vertexToLight = this->position - p;
+    //Calcul de l'aténuation de la lumière d'un vertex (linéaire)
+    float attenuation = std::max<float>(0.0f, 1.0f - (vertexToLight.length() / radius));
+
 
 }
