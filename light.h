f90698a428ca09f8ec6c36cae47587273ad40f64
Schmitt <clement.flance@gmail.com>
2015-01-11 20:22:04 +0100

    Ajout d'éléments dans la classe Light

--- light.h
+++ light.h
@@ -1,10 +1,31 @@
 #ifndef LIGHT_H
 #define LIGHT_H
 
-class light
+#include <QVector3D>
+#include <QString>
+#include <iostream>
+#include <QColor>
+
+class Light
 {
 public:
-    light();
+    Light();
+    ~Light();
+
+    float getRadius();
+    QColor getColor();
+    QVector3D getPosition();
+    QColor computeLighting(const QVector3D, const QVector3D);
+
+    void setRadius(float);
+    void setColor(int, int, int);
+    void setPosition(QVector3D);
+
+private:
+    float radius;
+    QColor color;
+    QVector3D position;
+
 };
 
 #endif // LIGHT_H
