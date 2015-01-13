d7d2bec8b1007e6444c300af2a735097c0a67051
mludwiczak <maryan.ludwiczak@gmail.com>
2015-01-13 01:23:38 +0100

    Light

--- gamewindow.cpp
+++ gamewindow.cpp
@@ -37,8 +37,8 @@ void GameWindow::initialize()
 
     glViewport(0, 0, width() * retinaScale, height() * retinaScale);
 
-    glClearColor(0.0, 0.0, 0.0, 0.0);
     glMatrixMode(GL_PROJECTION);
+    glClearColor(0.0, 0.0, 0.0, 0.0);
     glLoadIdentity();
     glOrtho(-10.0, 10.0, -10.0, 10.0, -10.0, 10.0);
 
@@ -46,19 +46,50 @@ void GameWindow::initialize()
     //level->addLight(light);
     //level->addLight(light);
 
+    glEnable(GL_DEPTH_TEST);
+    glEnable(GL_COLOR_MATERIAL);
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glEnable(GL_LIGHT1);
+    glEnable(GL_NORMALIZE);
+    glShadeModel(GL_SMOOTH);
+
     cam->setSS(0.1);
-    cam->setRotX(30);
-    cam->setRotY(30);
+    cam->setRotX(-45);
+    cam->setRotY(-45);
     cout<<"End of Initialization"<<endl;
 }
 
 void GameWindow::render()
 {
-    glClear(GL_COLOR_BUFFER_BIT);
-    glLoadIdentity();glScalef(cam->getSS(), cam->getSS(),cam->getSS());
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+
+    glScalef(cam->getSS(), cam->getSS(),cam->getSS());
     glRotatef(cam->getRotX(),1.0f,0.0f,0.0f);
     glRotatef(cam->getRotY(),0.0f,1.0f,0.0f);
     glTranslatef(-cam->getPos().x(), -cam->getPos().y(), -cam->getPos().z());
+
+    cout<<cam->getRotX()<<" "<<cam->getRotY()<<endl;
+
+    //Add ambient light
+    GLfloat ambientColor[] = {0.2f, 0.2f, 0.2f, 1.0f}; //Color (0.2, 0.2, 0.2)
+    glLightModelfv(GL_LIGHT_MODEL_AMBIENT, ambientColor);
+
+    //Add positioned light
+    GLfloat lightColor0[] = {0.5f, 0.5f, 0.5f, 1.0f}; //Color (0.5, 0.5, 0.5)
+    GLfloat lightPos0[] = {4.0f, 0.0f, 8.0f, 1.0f}; //Positioned at (4, 0, 8)
+    glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor0);
+    glLightfv(GL_LIGHT0, GL_POSITION, lightPos0);
+
+    //Add directed light
+    GLfloat lightColor1[] = {0.5f, 0.2f, 0.2f, 1.0f}; //Color (0.5, 0.2, 0.2)
+    //Coming from the direction (-1, 0.5, 0.5)
+    GLfloat lightPos1[] = {-1.0f, 0.5f, 0.5f, 0.0f};
+    glLightfv(GL_LIGHT1, GL_DIFFUSE, lightColor1);
+    glLightfv(GL_LIGHT1, GL_POSITION, lightPos1);
 
     //std::cout<<"X = "<<cam->getRotX()<<std::endl;
     //std::cout<<"Y = "<<cam->getRotY()<<std::endl;
@@ -128,6 +159,7 @@ void GameWindow::drawBall()
             //std::cout<<"Color red: "<< c.redF()<<std::endl;
 
             glVertex3f(p.x(), p.y(), p.z());
+            glNormal3f(n.x(), n.y(), n.z());
             glColor3f(0.7, 0.7, 0.7);
             //glColor3f(c.red()/255, c.green()/255, c.blue()/255);
 
@@ -157,10 +189,10 @@ void GameWindow::keyPressEvent(QKeyEvent *event)
     switch(event->key())
     {
     case Qt::Key_Up :
-        playerForce.setZ(playerForce.z()-10);
+        playerForce.setZ(playerForce.z()+10);
         break;
     case Qt::Key_Down :
-        playerForce.setZ(playerForce.z()+10);
+        playerForce.setZ(playerForce.z()-10);
         break;
     case Qt::Key_Left :
         playerForce.setX(playerForce.x()-10);
